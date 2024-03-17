efb03a855bebe4c12e04706535b12893954a7031
alitemel89 <64444827+alitemel89@users.noreply.github.com>
2024-03-17 12:39:54 +0300

    finish sidebar

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'web-app';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,ts}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [],
-  purge: ['./src/**/*.{html,ts}'],
-}
-
+  plugins: [require("flowbite/plugin")],
+};

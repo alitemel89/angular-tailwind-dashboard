5b5a4beff24ef71465667fc2a8d394611ecb1b99
alitemel89 <64444827+alitemel89@users.noreply.github.com>
2024-04-23 15:55:25 +0300

    add modal to contacts

--- client/src/app/contacts/contacts.component.ts
+++ client/src/app/contacts/contacts.component.ts
@@ -1,5 +1,8 @@
+// contacts.component.ts
+
 import { Component, OnInit } from '@angular/core';
 import { ContactService } from '../services/contact.service';
+
 
 @Component({
   selector: 'app-contacts',
@@ -7,13 +10,19 @@ import { ContactService } from '../services/contact.service';
 })
 export class ContactsComponent implements OnInit {
   contacts: Contact[] = [];
+  selectedContact: Contact | null = null;
 
   constructor(private contactService: ContactService) { }
-
 
   ngOnInit(): void {
     this.contacts = this.contactService.getContacts();
   }
+
+  openEditModal(contactId: number) {
+  // Find the contact by ID
+  this.selectedContact = this.contacts.find(contact => contact.id === contactId) || null;
+}
+
 }
 
 interface Contact {

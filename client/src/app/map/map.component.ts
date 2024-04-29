26addf7c34be7188f256f1b08e21021a6a1b4f70
alitemel89 <64444827+alitemel89@users.noreply.github.com>
2024-04-29 12:54:34 +0300

    add dark mode service to map

--- client/src/app/map/map.component.ts
+++ client/src/app/map/map.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { loadModules } from 'esri-loader';
-import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer";
-
-
+import GraphicsLayer from '@arcgis/core/layers/GraphicsLayer';
+import SimpleRenderer from '@arcgis/core/renderers/SimpleRenderer'; // Import SimpleRenderer class
+import SimpleFillSymbol from '@arcgis/core/symbols/SimpleFillSymbol'; // Import SimpleFillSymbol class
+import Color from '@arcgis/core/Color'; // Import Color class
+import { DarkModeService } from '../services/dark-mode.service';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  styleUrls: ['./map.component.css'],
 })
 export class MapComponent implements OnInit {
-  @ViewChild('mapViewNode', { static: true }) private mapViewEl!: ElementRef | undefined;
+  @ViewChild('mapViewNode', { static: true }) private mapViewEl!:
+    | ElementRef
+    | undefined;
+  private map: any;
 
-  constructor() { }
+  constructor(private darkModeService: DarkModeService) {}
 
   ngOnInit(): void {
     if (!this.mapViewEl) {
@@ -22,32 +27,52 @@ export class MapComponent implements OnInit {
 
     const graphicsLayer = new GraphicsLayer();
 
-    loadModules(['esri/Map', 'esri/views/MapView', 'esri/widgets/Sketch', 'esri/layers/FeatureLayer']).then(([Map, MapView, Sketch, FeatureLayer]) => {
-      const map = new Map({
-        basemap: 'topo-vector'
+    
+    loadModules(['esri/Map', 'esri/views/MapView', 'esri/widgets/Sketch', 'esri/layers/FeatureLayer'])
+    .then(([Map, MapView, Sketch, FeatureLayer]) => {
+      this.map = new Map({
+        basemap: 'streets-vector'
       });
 
       const view = new MapView({
         container: this.mapViewEl!.nativeElement,
-        map: map,
+        map: this.map,
         center: [10.4515, 51.1657],
-        zoom: 6
+
+        zoom: 6,
       });
 
       const layer = new FeatureLayer({
-        url: "https://services.arcgis.com/ue9rwulIoeLEI9bj/arcgis/rest/services/Germany%20Province%20Boundaries%202022%20update/FeatureServer"
+        url: 'https://services.arcgis.com/ue9rwulIoeLEI9bj/arcgis/rest/services/Germany%20Province%20Boundaries%202022%20update/FeatureServer',
+        renderer: new SimpleRenderer({
+          symbol: new SimpleFillSymbol({
+            color: new Color([0, 50, 150, 0.2]), // Set color with opacity
+            outline: {
+              color: [120, 0, 25, 0.2], // Set outline color with opacity
+              width: 1,
+            },
+          }),
+        }),
       });
-
       view.when(() => {
         const sketch = new Sketch({
           layer: graphicsLayer,
-          view: view
+          view: view,
         });
-        view.ui.add(sketch, "top-right");
+
+        view.ui.add(sketch, 'top-right');
 
         // Add layer to map
         view.map.add(layer);
       });
+    });
+
+    this.darkModeService.isDarkMode$.subscribe((isDarkMode) => {
+      if (isDarkMode) {
+        this.map.basemap = 'streets-night-vector';
+      } else {
+        this.map.basemap = 'streets-vector';
+      }
     });
   }
 }
